d1184289449662ad7cf44714a2dcb632e08fae76
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-08-03 15:01:30 +0000

    build: constroi topico no kafka com os dados extraidos da api do youtube

--- README.md
+++ README.md
@@ -4,6 +4,21 @@ Projeto de Streaming de dados do Youtube utilizando ecosistema Kafka e GCP.
      
 ## Como executar
      
+### 1. Crie um ambiente python e instale as dependências necessárias.
+```bash
+python -m venv .venv
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
+```         
+
+### 2. Crie o diretório config e dentro dele insira o arquivo .env contendo a seguinte estrutura.
+```bash
+API_KEY=<sua_chave_api>
+```   
+       
+### 3. Suba a estrutura Kafka em containeres Docker com o comando a seguir.
 ```bash
 docker compose up -d
 ```
--- main.py
+++ main.py
@@ -3,20 +3,39 @@ import os
 import json
 from dotenv import load_dotenv
 
+from pprint import pprint
+
+from kafka import KafkaProducer
+
 load_dotenv(dotenv_path='config/.env') # Deve-se criar o diretório e o arquivo conforme especificado
 API_KEY = os.getenv('API_KEY') # Deve-se armazenar a chave API dentro da variável API_KEY no arquivo '.env'
 
 url = 'https://www.googleapis.com/youtube/v3/videos'
 
 params = {
-    'part': 'snippet,statistics',
-    'chart': 'mostPopular',
-    'regionCode': 'BR', 
-    'maxResults': 5,
+    'id': 'AAS5UJiHQ4M',
+    'part': 'snippet, statistics, status',
     'key': API_KEY
 }
 
+
 if __name__ == '__main__':
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
     response = requests.get(url, params=params)
-    data = response.json()
-    print(json.dumps(data, indent=2))
+    data = json.loads(response.text)['items']
+
+    for video in data:
+        video_res = {
+            'title': video['snippet']['title'],
+            'likes': int(video['statistics'].get('likeCount', 0)),
+            'comments': int(video['statistics'].get('commentCount', 0)),
+            'views': int(video['statistics'].get('viewCount', 0)),
+            'favorites': int(video['statistics'].get('favoriteCount', 0)),
+            'thumbnail': video['snippet']['thumbnails']['default']['url']
+        }
+
+        pprint(video_res)
+
+        producer.send('youtube_videos', json.dumps(video_res).encode('utf-8'))
+        producer.flush()
